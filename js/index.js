1031952c265596041c9f6768540f5e22b7540b36
clz <1926389897@qq.com>
2021-09-30 09:55:34 +0800

    chouhua2

--- js/index.js
+++ js/index.js
@@ -65,7 +65,7 @@ start.addEventListener("click", () => {
     index++;
     if (index === sequence.length) {
       time += 50;
-      clearInterval(timer); //实现转速加快，先清除之前的，在开新的定时器
+      clearInterval(timer); //实现转速变慢，先清除之前的，在开新的定时器
       timer = setInterval(count, time);
       index = 0; //转完一圈回到起始点
     }
